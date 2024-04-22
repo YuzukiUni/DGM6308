f8070798b3e38ee969316cc9f6d6724e27e53016
YuzukiUni <103087594+YuzukiUni@users.noreply.github.com>
2024-04-22 05:06:59 -0400

    final

--- MatchGame/MatchGame/obj/Debug/net6.0/MatchGame.AssemblyInfo.cs
+++ MatchGame/MatchGame/obj/Debug/net6.0/MatchGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatchGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53389cc54b392cf0c8ecca8235977366a3ee9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f13ca355c75e10a318c8a2273da98503e825078")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatchGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatchGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
