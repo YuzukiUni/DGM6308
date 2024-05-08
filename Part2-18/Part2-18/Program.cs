c888cf3a68c6ddb316d26a87d329bda7042f5dee
YuzukiUni <103087594+YuzukiUni@users.noreply.github.com>
2024-05-07 23:56:42 -0400

    1

---
+++ Part2-18/Part2-18/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2_18
+{
+    internal class Program
+    {
+        public class DecreasingCounter
+        {
+            private int value;   // a variable that remembers the value of the counter
+
+            public DecreasingCounter(int initialValue)
+            {
+                this.value = initialValue;
+            }
+
+            public void PrintValue()
+            {
+                Console.WriteLine("value: " + this.value);
+            }
+
+            public void decrement()
+            {
+                if (this.value > 0)
+                {
+                    this.value--;
+                }
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            DecreasingCounter counter = new DecreasingCounter(10);
+            counter.PrintValue();
+
+            counter.decrement();
+            counter.PrintValue();
+
+            counter.decrement();
+            counter.PrintValue();
+        }
+    }
+}
